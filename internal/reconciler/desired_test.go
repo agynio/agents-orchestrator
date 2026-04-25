3b778af4908fb72906772ca71ce7f5b86e1a31dc
casey-brooks <casey.brooks@agyn.io>
2026-04-25 15:53:04 +0400

    fix(e2e): pin bootstrap ref (#164)
    * fix(e2e): pin bootstrap ref
    
    Add stub ListOrganizationThreads to fake client.
    
    * fix(e2e): pin provision action
    
    * chore(e2e): bump agent init image
    
    * chore(ci): bump bootstrap ref
    
    * chore(ci): pin e2e actions
    
    * test(reconciler): rename list params

--- internal/reconciler/desired_test.go
+++ internal/reconciler/desired_test.go
@@ -60,6 +60,9 @@ func (f *fakeThreadsClient) GetThreads(ctx context.Context, req *threadsv1.GetTh
 	}
 	return nil, testutil.ErrNotImplemented
 }
+func (f *fakeThreadsClient) ListOrganizationThreads(ctx context.Context, req *threadsv1.ListOrganizationThreadsRequest, opts ...grpc.CallOption) (*threadsv1.ListOrganizationThreadsResponse, error) {
+	return nil, testutil.ErrNotImplemented
+}
 func (f *fakeThreadsClient) GetThread(context.Context, *threadsv1.GetThreadRequest, ...grpc.CallOption) (*threadsv1.GetThreadResponse, error) {
 	return nil, testutil.ErrNotImplemented
 }
